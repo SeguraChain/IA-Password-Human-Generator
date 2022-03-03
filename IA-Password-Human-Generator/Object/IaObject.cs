4ba476ec2fea9cc1dd13a38b8922e5800e7e8d2b
Xenophyte <renaissancemt2@gmail.com>
2022-03-03 01:40:46 +0100

    Update IA
    Update.

---
+++ IA-Password-Human-Generator/Object/IaObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA_Password_Human_Generator.Object
+{
+    /// <summary>
+    /// IA object containing data from the scanned passwords report.
+    /// </summary>
+    public class IAPasswordData
+    {
+        public SortedDictionary<char, decimal> PasswordCharacterRank = new SortedDictionary<char, decimal>();
+        public SortedDictionary<decimal, decimal> PasswordLengthRank = new SortedDictionary<decimal, decimal>();
+        public SortedDictionary<decimal, SortedDictionary<char, decimal>> PasswordPosRank = new SortedDictionary<decimal, SortedDictionary<char, decimal>>();
+        public long TotalPassword;
+    }
+
+    /// <summary>
+    /// Used to contain lists of passwords generated from a report IA passwords previously generated.
+    /// </summary>
+    public class FileStreamObject
+    {
+        public string NameFile;
+        public FileStream FileStreamReader;
+        public FileStream FileStreamWriter;
+        public StreamWriter StreamWriter;
+        public StreamReader StreamReader;
+        public long Total;
+        public HashSet<string> ListWord;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public FileStreamObject()
+        {
+            ListWord = new HashSet<string>();
+        }
+    }
+
+    /// <summary>
+    /// Used to sort passwords from a file.
+    /// </summary>
+    public class StreamWriterObject
+    {
+        public FileStream FileStreamReader;
+        public FileStream FileStreamWriter;
+        public StreamWriter StreamWriter;
+        public StreamReader StreamReader;
+        public int Total;
+    }
+}
--- IA-Password-Human-Generator/Program.cs
+++ IA-Password-Human-Generator/Program.cs
@@ -10,6 +10,8 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using IA_Password_Human_Generator.Object;
+using IA_Password_Human_Generator.Utility;
 using Newtonsoft.Json;
 using Console = System.Console;
 
@@ -21,7 +23,7 @@ namespace IA_Password_Human_Generator
         /// <summary>
         /// Report of the password.
         /// </summary>
-        private static AIPasswordData _iaPasswordDataObject;
+        private static IAPasswordData _iaPasswordDataObject;
 
         /// <summary>
         /// Task.
@@ -52,50 +54,6 @@ namespace IA_Password_Human_Generator
 
 
 
-        /// <summary>
-        /// IA object containing data from the scanned passwords report.
-        /// </summary>
-        class AIPasswordData
-        {
-            public SortedDictionary<char, decimal> PasswordCharacterRank = new SortedDictionary<char, decimal>();
-            public SortedDictionary<decimal, decimal> PasswordLengthRank = new SortedDictionary<decimal, decimal>();
-            public SortedDictionary<decimal, SortedDictionary<char, decimal>> PasswordPosRank = new SortedDictionary<decimal, SortedDictionary<char, decimal>>();
-            public long TotalPassword;
-        }
-
-        /// <summary>
-        /// Used to contain lists of passwords generated from a report IA passwords previously generated.
-        /// </summary>
-        class FileStreamObject
-        {
-            public string NameFile;
-            public FileStream FileStreamReader;
-            public FileStream FileStreamWriter;
-            public StreamWriter StreamWriter;
-            public StreamReader StreamReader;
-            public long Total;
-            public HashSet<string> ListWord;
-
-            /// <summary>
-            /// Constructor.
-            /// </summary>
-            public FileStreamObject()
-            {
-                ListWord = new HashSet<string>();
-            }
-        }
-
-        /// <summary>
-        /// Used to sort passwords from a file.
-        /// </summary>
-        class StreamWriterObject
-        {
-            public FileStream FileStreamReader;
-            public FileStream FileStreamWriter;
-            public StreamWriter StreamWriter;
-            public StreamReader StreamReader;
-            public int Total;
-        }
 
 
         static void Main(string[] args)
@@ -109,7 +67,7 @@ namespace IA_Password_Human_Generator
             if (!Directory.Exists(AppContext.BaseDirectory + "\\" + BaseDirectoryPath + "\\"+DictionaryPath))
                 Directory.CreateDirectory(AppContext.BaseDirectory + "\\" + BaseDirectoryPath + "\\" + DictionaryPath);
 
-            AffichageMainMenu();
+            ShowMainMenu();
 
             Console.WriteLine("Press a key to close the program.");
             Console.ReadLine();
@@ -118,7 +76,7 @@ namespace IA_Password_Human_Generator
         /// <summary>
         /// Display of main menus.
         /// </summary>
-        private static void AffichageMainMenu()
+        private static void ShowMainMenu()
         {
             Console.WriteLine("Choose a function to use:");
             Console.WriteLine("1. Generate a report from a list of passwords.");
@@ -171,37 +129,37 @@ namespace IA_Password_Human_Generator
                 case "1":
                     BuildAiPasswordRapportData();
                     CleanUp();
-                    AffichageMainMenu();
+                    ShowMainMenu();
                     break;
                 case "2":
                     BuildPasswordFromAiRapportData();
                     CleanUp();
-                    AffichageMainMenu();
+                    ShowMainMenu();
                     break;
                 case "3":
                     RemoveDuplicateLineFromFile();
                     CleanUp();
-                    AffichageMainMenu();
+                    ShowMainMenu();
                     break;
                 case "4":
                     RemoveTooLowCharactersLengthLineFromFile();
                     CleanUp();
-                    AffichageMainMenu();
+                    ShowMainMenu();
                     break;
                 case "5":
                     CorrectPasswordList();
                     CleanUp();
-                    AffichageMainMenu();
+                    ShowMainMenu();
                     break;
                 case "6":
                     MergeFileInOne();
                     CleanUp();
-                    AffichageMainMenu();
+                    ShowMainMenu();
                     break;
                 case "7":
                     SplitFile();
                     CleanUp();
-                    AffichageMainMenu();
+                    ShowMainMenu();
                     break;
             }
         }
@@ -222,7 +180,7 @@ namespace IA_Password_Human_Generator
         /// </summary>
         private static void BuildAiPasswordRapportData()
         {
-            _iaPasswordDataObject = new AIPasswordData();
+            _iaPasswordDataObject = new IAPasswordData();
             Console.WriteLine("Choose the file who contain passwords:");
 
             string fichier = Console.ReadLine() ?? string.Empty;
@@ -302,7 +260,7 @@ namespace IA_Password_Human_Generator
             {
                 Console.WriteLine("Load of the report done..");
 
-                _iaPasswordDataObject = JsonConvert.DeserializeObject<AIPasswordData>(string.Concat(File.ReadAllLines(chemin)));
+                _iaPasswordDataObject = JsonConvert.DeserializeObject<IAPasswordData>(string.Concat(File.ReadAllLines(chemin)));
 
                 if (_iaPasswordDataObject == null)
                     Console.WriteLine("The file: " + chemin + " is empty or invalid, can't use the report content of this file.");
@@ -748,7 +706,7 @@ namespace IA_Password_Human_Generator
         }
 
         /// <summary>
-        /// Supprime les lignes dupliqué.
+        /// Remove duplicate rows.
         /// </summary>
         private static void RemoveDuplicateLineFromFile()
         {
@@ -1032,7 +990,7 @@ namespace IA_Password_Human_Generator
         }
 
         /// <summary>
-        /// Supprime les lignes qui ont un nombre de caractères inférieur à la limite choisi.
+        /// Deletes the lines which have a number of characters lower than the chosen limit.
         /// </summary>
         private static void RemoveTooLowCharactersLengthLineFromFile()
         {
@@ -1629,7 +1587,7 @@ namespace IA_Password_Human_Generator
         }
 
         /// <summary>
-        /// Fusionne des fichiers ciblés en un seul fichier.
+        /// Merge targeted files into a single file.
         /// </summary>
         private static void MergeFileInOne()
         {
@@ -2904,7 +2862,7 @@ namespace IA_Password_Human_Generator
         }
 
         /// <summary>
-        /// Découpe un fichier en plusieurs parties.
+        /// Split a file into several parts.
         /// </summary>
         private static void SplitFile()
         {
@@ -4420,400 +4378,4 @@ namespace IA_Password_Human_Generator
         #endregion
     }
 
-    #region Other class.
-
-    /// <summary>
-    /// A great class to detect at maximum, the real encoding of the content of a string.
-    /// This one have help me to select the right Encoding class to use.
-    /// </summary>
-    public class TextEncodingDetect
-    {
-        private readonly byte[] _utf16BeBom =
-        {
-            0xFE,
-            0xFF
-        };
-
-        private readonly byte[] _utf16LeBom =
-        {
-            0xFF,
-            0xFE
-        };
-
-        private readonly byte[] _utf8Bom =
-        {
-            0xEF,
-            0xBB,
-            0xBF
-        };
-
-        private bool _nullSuggestsBinary = true;
-        private double _utf16ExpectedNullPercent = 70;
-        private double _utf16UnexpectedNullPercent = 10;
-
-        public enum Encoding
-        {
-            None, // Unknown or binary
-            Ansi, // 0-255
-            Ascii, // 0-127
-            Utf8Bom, // UTF8 with BOM
-            Utf8Nobom, // UTF8 without BOM
-            Utf16LeBom, // UTF16 LE with BOM
-            Utf16LeNoBom, // UTF16 LE without BOM
-            Utf16BeBom, // UTF16-BE with BOM
-            Utf16BeNoBom // UTF16-BE without BOM
-        }
-
-        /// <summary>
-        ///     Sets if the presence of nulls in a buffer indicate the buffer is binary data rather than text.
-        /// </summary>
-        public bool NullSuggestsBinary
-        {
-            set
-            {
-                _nullSuggestsBinary = value;
-            }
-        }
-
-        public double Utf16ExpectedNullPercent
-        {
-            set
-            {
-                if (value > 0 && value < 100)
-                {
-                    _utf16ExpectedNullPercent = value;
-                }
-            }
-        }
-
-        public double Utf16UnexpectedNullPercent
-        {
-            set
-            {
-                if (value > 0 && value < 100)
-                {
-                    _utf16UnexpectedNullPercent = value;
-                }
-            }
-        }
-
-        /// <summary>
-        ///     Gets the BOM length for a given Encoding mode.
-        /// </summary>
-        /// <param name="encoding"></param>
-        /// <returns>The BOM length.</returns>
-        public static int GetBomLengthFromEncodingMode(Encoding encoding)
-        {
-            int length;
-
-            switch (encoding)
-            {
-                case Encoding.Utf16BeBom:
-                case Encoding.Utf16LeBom:
-                    length = 2;
-                    break;
-
-                case Encoding.Utf8Bom:
-                    length = 3;
-                    break;
-
-                default:
-                    length = 0;
-                    break;
-            }
-
-            return length;
-        }
-
-        /// <summary>
-        ///     Checks for a BOM sequence in a byte buffer.
-        /// </summary>
-        /// <param name="buffer"></param>
-        /// <param name="size"></param>
-        /// <returns>Encoding type or Encoding.None if no BOM.</returns>
-        public Encoding CheckBom(byte[] buffer, int size)
-        {
-            // Check for BOM
-            if (size >= 2 && buffer[0] == _utf16LeBom[0] && buffer[1] == _utf16LeBom[1])
-            {
-                return Encoding.Utf16LeBom;
-            }
-
-            if (size >= 2 && buffer[0] == _utf16BeBom[0] && buffer[1] == _utf16BeBom[1])
-            {
-                return Encoding.Utf16BeBom;
-            }
-
-            if (size >= 3 && buffer[0] == _utf8Bom[0] && buffer[1] == _utf8Bom[1] && buffer[2] == _utf8Bom[2])
-            {
-                return Encoding.Utf8Bom;
-            }
-
-            return Encoding.None;
-        }
-
-        /// <summary>
-        ///     Automatically detects the Encoding type of a given byte buffer.
-        /// </summary>
-        /// <param name="buffer">The byte buffer.</param>
-        /// <param name="size">The size of the byte buffer.</param>
-        /// <returns>The Encoding type or Encoding.None if unknown.</returns>
-        public Encoding DetectEncoding(byte[] buffer, int size)
-        {
-            // First check if we have a BOM and return that if so
-            Encoding encoding = CheckBom(buffer, size);
-            if (encoding != Encoding.None)
-            {
-                return encoding;
-            }
-
-            // Now check for valid UTF8
-            encoding = CheckUtf8(buffer, size);
-            if (encoding != Encoding.None)
-            {
-                return encoding;
-            }
-
-            // Now try UTF16 
-            encoding = CheckUtf16NewlineChars(buffer, size);
-            if (encoding != Encoding.None)
-            {
-                return encoding;
-            }
-
-            encoding = CheckUtf16Ascii(buffer, size);
-            if (encoding != Encoding.None)
-            {
-                return encoding;
-            }
-
-            // ANSI or None (binary) then
-            if (!DoesContainNulls(buffer, size))
-            {
-                return Encoding.Ansi;
-            }
-
-            // Found a null, return based on the preference in null_suggests_binary_
-            return _nullSuggestsBinary ? Encoding.None : Encoding.Ansi;
-        }
-
-        /// <summary>
-        ///     Checks if a buffer contains text that looks like utf16 by scanning for
-        ///     newline chars that would be present even in non-english text.
-        /// </summary>
-        /// <param name="buffer">The byte buffer.</param>
-        /// <param name="size">The size of the byte buffer.</param>
-        /// <returns>Encoding.none, Encoding.Utf16LeNoBom or Encoding.Utf16BeNoBom.</returns>
-        private static Encoding CheckUtf16NewlineChars(byte[] buffer, int size)
-        {
-            if (size < 2)
-            {
-                return Encoding.None;
-            }
-
-            // Reduce size by 1 so we don't need to worry about bounds checking for pairs of bytes
-            size--;
-
-            var leControlChars = 0;
-            var beControlChars = 0;
-
-            uint pos = 0;
-            while (pos < size)
-            {
-                byte ch1 = buffer[pos++];
-                byte ch2 = buffer[pos++];
-
-                if (ch1 == 0)
-                {
-                    if (ch2 == 0x0a || ch2 == 0x0d)
-                    {
-                        ++beControlChars;
-                    }
-                }
-                else if (ch2 == 0)
-                {
-                    if (ch1 == 0x0a || ch1 == 0x0d)
-                    {
-                        ++leControlChars;
-                    }
-                }
-
-                // If we are getting both LE and BE control chars then this file is not utf16
-                if (leControlChars > 0 && beControlChars > 0)
-                {
-                    return Encoding.None;
-                }
-            }
-
-            if (leControlChars > 0)
-            {
-                return Encoding.Utf16LeNoBom;
-            }
-
-            return beControlChars > 0 ? Encoding.Utf16BeNoBom : Encoding.None;
-        }
-
-        /// <summary>
-        /// Checks if a buffer contains any nulls. Used to check for binary vs text data.
-        /// </summary>
-        /// <param name="buffer">The byte buffer.</param>
-        /// <param name="size">The size of the byte buffer.</param>
-        private static bool DoesContainNulls(byte[] buffer, int size)
-        {
-            uint pos = 0;
-            while (pos < size)
-            {
-                if (buffer[pos++] == 0)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        ///     Checks if a buffer contains text that looks like utf16. This is done based
-        ///     on the use of nulls which in ASCII/script like text can be useful to identify.
-        /// </summary>
-        /// <param name="buffer">The byte buffer.</param>
-        /// <param name="size">The size of the byte buffer.</param>
-        /// <returns>Encoding.none, Encoding.Utf16LeNoBom or Encoding.Utf16BeNoBom.</returns>
-        private Encoding CheckUtf16Ascii(byte[] buffer, int size)
-        {
-            var numOddNulls = 0;
-            var numEvenNulls = 0;
-
-            // Get even nulls
-            uint pos = 0;
-            while (pos < size)
-            {
-                if (buffer[pos] == 0)
-                {
-                    numEvenNulls++;
-                }
-
-                pos += 2;
-            }
-
-            // Get odd nulls
-            pos = 1;
-            while (pos < size)
-            {
-                if (buffer[pos] == 0)
-                {
-                    numOddNulls++;
-                }
-
-                pos += 2;
-            }
-
-            double evenNullThreshold = numEvenNulls * 2.0 / size;
-            double oddNullThreshold = numOddNulls * 2.0 / size;
-            double expectedNullThreshold = _utf16ExpectedNullPercent / 100.0;
-            double unexpectedNullThreshold = _utf16UnexpectedNullPercent / 100.0;
-
-            // Lots of odd nulls, low number of even nulls
-            if (evenNullThreshold < unexpectedNullThreshold && oddNullThreshold > expectedNullThreshold)
-            {
-                return Encoding.Utf16LeNoBom;
-            }
-
-            // Lots of even nulls, low number of odd nulls
-            if (oddNullThreshold < unexpectedNullThreshold && evenNullThreshold > expectedNullThreshold)
-            {
-                return Encoding.Utf16BeNoBom;
-            }
-
-            // Don't know
-            return Encoding.None;
-        }
-
-        /// <summary>
-        ///     Checks if a buffer contains valid utf8.
-        /// </summary>
-        /// <param name="buffer">The byte buffer.</param>
-        /// <param name="size">The size of the byte buffer.</param>
-        /// <returns>
-        ///     Encoding type of Encoding.None (invalid UTF8), Encoding.Utf8NoBom (valid utf8 multibyte strings) or
-        ///     Encoding.ASCII (data in 0.127 range).
-        /// </returns>
-        /// <returns>2</returns>
-        private Encoding CheckUtf8(byte[] buffer, int size)
-        {
-            // UTF8 Valid sequences
-            // 0xxxxxxx  ASCII
-            // 110xxxxx 10xxxxxx  2-byte
-            // 1110xxxx 10xxxxxx 10xxxxxx  3-byte
-            // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx  4-byte
-            //
-            // Width in UTF8
-            // Decimal      Width
-            // 0-127        1 byte
-            // 194-223      2 bytes
-            // 224-239      3 bytes
-            // 240-244      4 bytes
-            //
-            // Subsequent chars are in the range 128-191
-            var onlySawAsciiRange = true;
-            uint pos = 0;
-
-            while (pos < size)
-            {
-                byte ch = buffer[pos++];
-
-                if (ch == 0 && _nullSuggestsBinary)
-                {
-                    return Encoding.None;
-                }
-
-                int moreChars;
-                if (ch <= 127)
-                {
-                    // 1 byte
-                    moreChars = 0;
-                }
-                else if (ch >= 194 && ch <= 223)
-                {
-                    // 2 Byte
-                    moreChars = 1;
-                }
-                else if (ch >= 224 && ch <= 239)
-                {
-                    // 3 Byte
-                    moreChars = 2;
-                }
-                else if (ch >= 240 && ch <= 244)
-                {
-                    // 4 Byte
-                    moreChars = 3;
-                }
-                else
-                {
-                    return Encoding.None; // Not utf8
-                }
-
-                // Check secondary chars are in range if we are expecting any
-                while (moreChars > 0 && pos < size)
-                {
-                    onlySawAsciiRange = false; // Seen non-ascii chars now
-
-                    ch = buffer[pos++];
-                    if (ch < 128 || ch > 191)
-                    {
-                        return Encoding.None; // Not utf8
-                    }
-
-                    --moreChars;
-                }
-            }
-
-            // If we get to here then only valid UTF-8 sequences have been processed
-
-            // If we only saw chars in the range 0-127 then we can't assume UTF8 (the caller will need to decide)
-            return onlySawAsciiRange ? Encoding.Ascii : Encoding.Utf8Nobom;
-        }
-    }
-
-    #endregion
 }
